a2c5316c3e9502b0be3942f9cc7f1e05cc77f75d
Daniel Thompson <19dst89@gmail.com>
2017-01-21 22:24:29 -0800

    basic conditional if statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,10 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+  alert("You found a match!");
+}
+  else {
+    alert("Sorry try again")
+  };
