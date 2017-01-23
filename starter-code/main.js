203d3d7cb500722a75faade6931f93302c09238a
Daniel Thompson <19dst89@gmail.com>
2017-01-23 00:40:37 -0800

    added create cards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,27 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
 
-if (cardTwo === cardFour) {
-  alert("You found a match!");
-}
-  else {
-    alert("Sorry try again")
-  };
+var cards = Array("queen", "king", "queen", "king")
+
+// if (cardTwo === cardFour) {
+//   alert("You found a match!");
+// }
+//   else {
+//     alert("Sorry try again")
+//   };
+var createCards = function(cards) {
+
+  var gBoard = document.getElementById('game-board');
+
+  for (var i = cards.length - 1; i >= 0; i--) {
+    var newDiv = document.createElement('div');
+    newDiv.textContent = cards[i];
+    gBoard.appendChild(newDiv).className = 'card';
+
+  }};
+
+  createCards(cards);
